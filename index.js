0f2483b710747927a754b0ef9fc19df2180de836
mostsfa1282006 <ma5745768@gmail.com>
2025-06-02 10:16:26 +0300

    version 1

---
+++ index.js
@@ -0,0 +1,7 @@
+let fatora = +prompt("Enter value of fatora ");
+let increase = prompt("Enter value of increase ");
+let vat = (increase / 100) * fatora;
+let final = vat + fatora;
+console.log("original value: " + fatora);
+console.log("original increase: " + increase);
+console.log("value of fatora after increase: " + final);
